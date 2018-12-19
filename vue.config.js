138a1545a6dbae7cee1918b3d7fc2ec622740284
Caleb Roseland <caleb@calebroseland.com>
2018-12-18 21:19:42 -0600

    chore: scaffolding update

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  pwa: {
+    name: 'CalebRoseland.com'
+  },
+
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    sourceMap: true
+  }
+}
